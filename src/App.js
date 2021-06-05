c53191316cadd0867592da97e9629dca15613522
ravi-tesh <raviteshchaudhary551@gmail.com>
2021-06-05 23:53:30 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { Form, Input, Button, Row } from 'antd';
+import FormItem from 'antd/lib/form/FormItem';
+import 'antd/dist/antd.css';
 
 function App() {
+  const handleSubmit = (values) => {
+    console.log(values.num);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <h1>Proedge Assignment Application</h1>
+      <Form onFinish={handleSubmit} layout='vertical'>
+        <FormItem
+          name='num'
+          rules={[
+            {
+              required: true,
+              type: 'string',
+              message:
+                'Please enter the number with ,'
+            },
+          ]}
+          hasFeedback
         >
-          Learn React
-        </a>
-      </header>
+          <Input
+            size='large'
+            placeholder='Enter the number'
+          />
+        </FormItem>
+
+        <Row>
+          <Button
+            style={{ backgroundColor: 'blue', color: '#fff' }}
+            htmlType='submit'
+            size='large'
+            block
+          >
+            Submit
+          </Button>
+        </Row>
+      </Form>
     </div>
   );
 }
